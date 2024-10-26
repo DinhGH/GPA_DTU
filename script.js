c6bf375396973000f6680bceb67c83feab36477b
Dinh-github1 <huynhtandinh@dtu.edu.vn>
2024-10-26 10:44:27 +0700

    last version

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 var inp = document.getElementsByTagName('input');
 var opt = document.getElementsByTagName('option');
-
+var disp = document.getElementById('md');
+var redi = document.getElementById('redi');
 
 function cal(){
     for(let i=2; i< inp.length-1; i=i+3) {
-        inp[i].value = inp[i-1].value * inp[i-2].value;
+        inp[i].value = (inp[i-1].value * inp[i-2].value).toFixed(2);
     }
 }
 
-function gpa(){ 
-    let total = 0;
-    for(let i=2; i< inp.length-1; i=i+3) {
-        total += Number(inp[i].value);
+var gpac = 0;
+function gpa(x){ 
+    gpac = x.value;
+}
+
+function gpa1(){
+    let sd = 0;
+    for(let i=0; i< inp.length-1; i++) {
+        if(i%3==2)
+            sd += Number(inp[i].value);
     }
-    inp[inp.length-1].value = total;
+    inp[inp.length-1].value = sd.toFixed(2);
 }
 
 function cgpa(){
-    let gpa = 0;
-    for(let i=1; i< opt.length; i++) {
-        gpa += Number(opt[i].value);
-    }
     let sp = 0, sd = 0;
     for(let i=0; i< inp.length-1; i++) {
         if(i%3==0 || i==0)
@@ -28,6 +31,19 @@ function cgpa(){
         if(i%3==2)
             sd += Number(inp[i].value);
     }
-    alert("sp: " + sp + " sd: " + sd + " gpa: " + gpa);
-    inp[inp.length-1].value = ((gpa - sd) / (1-sp));
+    // alert(gpac+""+sd+""+sp);
+    redi.innerHTML= "Bạn phải hoàn thành các cột còn lại với "+((gpac - sd) / (1-sp)).toFixed(2)+" điểm";
+    disp.showModal();
+}
+function overp(x){
+    if(x.value >1 || x.value < 0)
+        alert("Vượt quá giới hạn: 0.00 -> 1.00\nVí dụ: 5% = 0.05; 10% = 0.1; 15% = 0.15");
+}
+function overd(x){
+    if(x.value >10 || x.value < 0)
+        alert("Vượt quá giới hạn: 0.00 -> 10.00");
+   
+}
+function exit(){
+    disp.close();
 }
